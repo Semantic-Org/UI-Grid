7cf448fe1514a16a0931acf9eb7c90cbc4579c5d
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:25:07 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-grid',
+  summary : 'Semantic UI - Grid (official): Single component release of grid',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Grid.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
