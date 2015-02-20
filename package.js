6f83e571bf4c7c57ff231f2dd41e5c04cbc6c4fa
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:43 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-grid',
-  summary : 'Semantic UI - Grid (official): Single component release of grid',
-  version : '1.9.2',
+  summary : 'Semantic UI - Grid: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Grid.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'grid.css'
+  ], 'client');
 });
