41ab07b1556f414a1bc2b5a81a55222153843a37
Pusher Robot <jack@semantic-ui.com>
2015-03-27 12:31:11 -0400

    Updated component to version 1.11.6

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-grid',
   summary : 'Semantic UI - Grid: Single component release',
-  version : '1.11.5',
+  version : '1.11.6',
   git     : 'git://github.com/Semantic-Org/UI-Grid.git',
 });
 
