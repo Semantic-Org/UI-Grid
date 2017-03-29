b21e2f882356b4010542f34945a89f45a2bcc284
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:41:26 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-grid',
   summary : 'Semantic UI - Grid: Single component release',
-  version : '2.2.9',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Grid.git',
 });
 
